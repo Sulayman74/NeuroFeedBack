e9811676ee1a8193f46c0b257c4538f5fd761240
Sulayman <sirikone@hotmail.com>
2022-07-31 23:37:07 +0200

    ajoutFilesSK

--- Sulayman_SK/testPopUp/app.js
+++ Sulayman_SK/testPopUp/app.js
@@ -1,3 +1,23 @@
+let onButtons = document.querySelectorAll(".btnOn");
+let btnsEnSavoirPlus = document.querySelectorAll(".btnChange");
+
+console.log(onButtons);
+console.log(btnsEnSavoirPlus);
+
+onButtons.forEach((BtnOn) => {
+  console.log();
+  BtnOn.addEventListener("mouseenter", function (e) {
+    BtnOn.closest(".Btns").querySelector(".btnChange").style.color = "#022b88";
+    BtnOn.closest(".Btns").querySelector(".btnChange").style.backgroundColor =
+      "#fff";
+  });
+  BtnOn.addEventListener("mouseleave", function (e) {
+    BtnOn.closest(".Btns").querySelector(".btnChange").style.color = "#fff";
+    BtnOn.closest(".Btns").querySelector(".btnChange").style.backgroundColor =
+      "#022b88";
+  });
+});
+
 let openModalBtns = document.querySelectorAll("[data-modal-target]");
 let closeModalBtns = document.querySelectorAll("[data-close-button]");
 let overlay = document.getElementById("overlay");
