fb4ad229b4bfd3491c1148dca7abd0ad83605e31
fouad <m.fouad@outlook.fr>
2022-08-19 10:37:01 +0200

    fouad

--- Fouad/NeuroFeedBack- Copie/js/script.js
+++ Fouad/NeuroFeedBack- Copie/js/script.js
@@ -1,3 +1,19 @@
+// ------ btn go up... ------
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 150) $(".gotopbtn").addClass("active");
+  else $(".gotopbtn").removeClass("active");
+});
+$(".gotopbtn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 400);
+});
+
+// ------ menu-color : noir et blanc... ------
+
+window.addEventListener('scroll', function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+})
+
 // ------ Start scripte scroll Trigger - Video + mini img...  ------
 
 gsap.to("#videoplyr", {
@@ -52,22 +68,86 @@ gsap.to("#holdr2", {
 
 // ------ End scripte scroll Trigger - Video + mini img...  ------
 
-const open = document.querySelector(".container");
+//__---Start scripte GSAP Menu !!!
 
-const close = document.querySelector(".close");
-var t1 = gsap.timeline({ defaults: { duration: 1, ease: "expo.inOut" } });
-open.addEventListener("click", () => {
-  if (t1.reversed()) {
-    t1.play();
-  } else {
-    t1.to("nav", { right: 0 })
-      .to("nav", { height: "100vh" }, "-=.1")
-      .to("nav ul a", { opacity: 1, PointerEvent: "all", stagger: 0.2 }, "-=.8")
-      .to(".close", { opacity: 1, PointerEvent: "all" }, "-=.8")
-      .to("nav", { opacity: 1 }, "-=1");
-  }
+var t1 = gsap.timeline({ paused: true });
+
+var menuToggle = document.getElementById("menuToggle");
+var menuBar = gsap.timeline();
+
+menuBar.to(
+  ".bar-1",
+  0.5,
+  {
+    attr: { d: "M8,2 L2,8" },
+    x: 1,
+    ease: Power2.easeInOut,
+  },
+  "start"
+);
+
+menuBar.to(
+  ".bar-2",
+  0.5,
+  {
+    autoAlpha: 0,
+  },
+  "start"
+);
+
+menuBar.to(
+  ".bar-3",
+  0.5,
+  {
+    attr: { d: "M8,8 L2,2" },
+    x: 1,
+    ease: Power2.easeInOut,
+  },
+  "start"
+);
+
+menuToggle.addEventListener("click", function () {
+  menuBar.reversed(!menuBar.reversed());
+  t1.reversed(!t1.reversed());
 });
 
-close.addEventListener("click", () => {
-  t1.reverse();
+menuBar.reverse();
+t1.reverse();
+
+t1.to(".fullpage-menu", {
+  duration: 0,
+  display: "block",
+  ease: "Expo.easeInOut",
 });
+
+t1.from(".menu-bg span", {
+  duration: 0.6,
+  x: "100%",
+  stagger: 0.1,
+  ease: "Expo.easeInOut",
+});
+
+t1.from(
+  ".main-menu li a",
+  {
+    duration: 0.8,
+    y: "100%",
+    stagger: 0.2,
+    ease: "Expo.easeInOut",
+  },
+  "-=0.5"
+);
+
+t1.from(
+  ".social-links li",
+  {
+    duration: 0.7,
+    y: "-100%",
+    opacity: 0,
+    stagger: 0.1,
+    ease: "Expo.easeInOut",
+  },
+  "-=0.5"
+);
+
+//__---End scripte GSAP Menu !!!
