4bd7b764f61abe5f847cb205c7f34a608eef4396
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2022-09-08 09:21:40 +0200

    Version 2 de Fouad

--- Fouad/NeuroFeedBack- Copie/js/1btn-to-upJQ.js
+++
@@ -1,9 +0,0 @@
-// ------ btn go up... ------
-$(window).scroll(function () {
-    if ($(this).scrollTop() > 150) $(".gotopbtn").addClass("active");
-    else $(".gotopbtn").removeClass("active");
-});
-$(".gotopbtn").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 400);
-});
-
