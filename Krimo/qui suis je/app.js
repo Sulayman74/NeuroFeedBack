0ac2b95073896269f4922f80bb165e3d48138d98
Abdelkrim <kissab@gmail.com>
2022-09-15 20:29:07 +0200

    Modifs seance html et ajout animation dans qui suis je

--- Krimo/qui suis je/app.js
+++ Krimo/qui suis je/app.js
@@ -22,3 +22,32 @@ gsap.fromTo(
         },
     }
 );
+
+"use strict";
+
+const hello = document.querySelector(".hello");
+const divs = hello.querySelectorAll("img");
+
+const options = {
+  rootMargin: "0",
+  threshold: 0.5,
+};
+
+const observer = new IntersectionObserver(entries => {
+  console.log(entries);
+
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log(entry.target);
+    //   entry.target.style.transform = "scale(2.5)";
+      entry.target.classList.add("active");
+    } else if (entry.target.classList.contains("active")) {
+      //   entry.target.style.transform = "scale(1)";
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+const item = divs.forEach(div => {
+  observer.observe(div, options);
+});
