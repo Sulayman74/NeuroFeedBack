62b4a7650b72790e6ac524afbb2b673607321921
fouad <m.fouad@outlook.fr>
2022-09-19 11:21:30 +0200

    animation BIO

---
+++ Fouad/biofeedback/animation_fb_info.js
@@ -0,0 +1,51 @@
+TweenMax.from(".logo", 1.6, {
+    delay: 6.4,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".menu", 1.6, {
+    delay: 6.5,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".heading", 1.6, {
+    delay: 6.4,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".desc", 1.6, {
+    delay: 6.7,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from("button", 1.6, {
+    delay: 6.8,
+    opacity: 0,
+    y: 30,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".watch", 1.6, {
+    delay: 6.9,
+    opacity: 0,
+    y: 30,
+    rotation: 90,
+    ease: Expo.easeInOut
+});
+
+
+TweenMax.staggerFrom(".media ul li", 2, {
+    delay: 7,
+    opacity: 0,
+    y: 40,
+    ease: Expo.easeInOut
+}, 0.2);
+
